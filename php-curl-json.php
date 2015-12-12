fc5dc7bb0cea8c6f606935e49950452437015d13
Muhamad Fajar <therevance@gmail.com>
2015-12-13 00:32:24 +0700

    Edit var throw using array

--- php-curl-json.php
+++ php-curl-json.php
@@ -1,19 +1,19 @@
 <?php
 class phpCurl{
-  public function rest($endpoint = '', $method = '', $var = '', $optional = '', $header = ''){
+  public function rest($data){
     // initiated variable curl
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $endpoint);
-    if($method === 'post'){
+    curl_setopt($ch, CURLOPT_URL, $data['endpoint']);
+    if($data['method'] === 'post'){
       // post data
       curl_setopt($ch, CURLOPT_POST, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($var));
+      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data['variable']));
     }
     // set data inside header
-    curl_setopt($ch, CURLOPT_HEADER, $header);
+    curl_setopt($ch, CURLOPT_HEADER, $data['header']);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     if(isset($optional)){
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $optional);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $data['http_header']);
     }
     // return data into json
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
