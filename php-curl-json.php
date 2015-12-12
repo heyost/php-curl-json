0b8733423ccc44eceeff711f3fd8addf35bf7799
Muhamad Fajar <therevance@gmail.com>
2015-12-12 23:37:07 +0700

    Added POST method

--- php-curl-json.php
+++ php-curl-json.php
@@ -3,14 +3,18 @@ class phpCurl{
   public function rest($endpoint = '', $method = '', $var = '', $optional = ''){
     // initiated variable curl
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $api_var ."verification/auth.do");
-    // post data
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('username' => $_POST['email'], 'password' => $_POST['password'])));
+    curl_setopt($ch, CURLOPT_URL, $endpoint);
+    if($method === 'post'){
+      // post data
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($var));
+    }
     // set data inside header
     curl_setopt($ch, CURLOPT_HEADER, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('TOKEN: 123'));
+    if(isset($optional)){
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $optional);  
+    }
     // return data into json
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $server_output = curl_exec($ch);
