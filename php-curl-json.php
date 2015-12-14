8f883df9b955b9697c83aff547026adf3467150b
Muhamad Fajar <therevance@gmail.com>
2015-12-14 11:22:52 +0700

    Fixing bad variable fetch

--- php-curl-json.php
+++ php-curl-json.php
@@ -12,7 +12,7 @@ class phpCurl{
     // set data inside header
     curl_setopt($ch, CURLOPT_HEADER, $data['header']);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    if($data['http_header']){
+    if(@$data['http_header']){
       curl_setopt($ch, CURLOPT_HTTPHEADER, $data['http_header']);
     }
     // return data into json
